d0a40803e4155adafd310275de71e0db0ad12eda
Rene Gomez <renegomez@yahoo.com>
2017-12-30 16:44:50 -0500

    "Practice console app using comand line args."

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what's your name?");
+            string name = Console.ReadLine(); 
+            Console.WriteLine("this is a test, " + name);
+
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if(age < 20)
+            {
+                Console.WriteLine("Wow, you are young!");
+            }
+            else
+            {
+                Console.WriteLine("Wow, you are old");
+            }
+        }
+    }
+}
